3a96a6f58f78904649b910a51ff756c2ae3f192c
Rithvik <rbhogavilli@gmail.com>
2020-09-23 09:29:04 -0400

    Install FTCLib and implement intake subsystem code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/YellowJacket435.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode.Hardware;
+
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class YellowJacket435 implements Motor {
+    private DcMotor m_motor;
+    private double resetVal;
+
+    public static final double TICKS_PER_REV = 383.6;
+
+    public YellowJacket435(HardwareMap hMap, String name) {
+        m_motor = hMap.get(DcMotor.class, name);
+    }
+
+    @Override
+    public void set(double speed) {
+        m_motor.setPower(speed);
+    }
+
+    @Override
+    public double get() {
+        return m_motor.getPower();
+    }
+
+    @Override
+    public void setInverted(boolean isInverted) {
+        m_motor.setDirection(!isInverted ? DcMotor.Direction.FORWARD : DcMotor.Direction.REVERSE);
+    }
+
+    @Override
+    public boolean getInverted() {
+        return m_motor.getDirection() == DcMotor.Direction.REVERSE;
+    }
+
+    @Override
+    public void disable() {
+        m_motor.close();
+    }
+
+    @Override
+    public String getDeviceType() {
+        return m_motor.getDeviceName() + " connected at " + m_motor.getPortNumber();
+    }
+
+    @Override
+    public void pidWrite(double output) {
+        set(output);
+    }
+
+    @Override
+    public void stopMotor() {
+        set(0);
+    }
+
+    public double getEncoderCount() {
+        return m_motor.getCurrentPosition() - resetVal;
+    }
+
+    public void resetEncoder() {
+        resetVal += getEncoderCount();
+    }
+
+    public double getNumRevolutions() {
+        return getEncoderCount() / TICKS_PER_REV;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Intake.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.Hardware.YellowJacket435;
+
+public class Intake extends SubsystemBase {
+    private final YellowJacket435 intakeMotor;
+
+    public Intake(final HardwareMap map, String name) {
+        intakeMotor = new YellowJacket435(map, "intakeMotor");
+    }
+
+    public void setPower(double power) {
+        intakeMotor.set(power);
+    }
+}
