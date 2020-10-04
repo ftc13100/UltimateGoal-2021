4b2d91fe93439be28dd2613f436fdebf61733402
Rithvik <rbhogavilli@gmail.com>
2020-10-04 12:27:08 -0400

    Drivetrain and shooter commands

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/DefaultDrive.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.commands;
+
+class DefaultDrive extends CommandBase {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/RunShooter.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.commands;
+
+class RunShooter extends CommandBase {
+}
