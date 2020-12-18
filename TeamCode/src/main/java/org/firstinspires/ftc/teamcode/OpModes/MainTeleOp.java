29cacda84d5b00801d32690bf5707704e3f2e70d
Rithvik <rbhogavilli@gmail.com>
2020-12-18 13:37:20 -0500

    Clear OpMode in master branch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/MainTeleOp.java
@@ -1,24 +1,13 @@
 package org.firstinspires.ftc.teamcode.opModes;
 
+import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
-import org.firstinspires.ftc.teamcode.BeaverRobot;
 
 @TeleOp(name="TeleOp")
-public class MainTeleOp extends LinearOpMode {
-
-    BeaverRobot robot = new BeaverRobot(this, true);
+public class MainTeleOp extends CommandOpMode {
 
     @Override
-    public void runOpMode() throws InterruptedException {
-        waitForStart();
+    public void initialize() {
 
-        while (opModeIsActive() && !isStopRequested()) {
-            //the scheduler will keep track of all of the triggers we made
-            robot.run();
-        }
-
-        robot.reset();
     }
 }
